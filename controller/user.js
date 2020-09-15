b6c9fa4b770657cdfe5c548c80404e7094a7d023
mainu007 <mainudev.co@gmail.com>
2020-09-15 08:55:21 +0600

    get single user method and added user route "/user"

--- controller/user.js
+++ controller/user.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 //show all users
 exports.allUsers = (req, res) => {
    User.find()
+      .select("_id name email")
       .then((users) => {
          return res.json(users);
       })
@@ -11,4 +12,29 @@ exports.allUsers = (req, res) => {
             error: err,
          });
       });
+};
+
+//show single user
+exports.getUser = (req, res) => {
+   User.findOne({ email: req.profile.email }, (err, user) => {
+      if (err || !user) {
+         return res.status(401).json({
+            error: err,
+         });
+      }
+      res.json(user);
+   }).select("_id name email");
+};
+
+//create req.profile method
+exports.userById = (req, res, next, id) => {
+   User.findById(id).exec((err, user) => {
+      if (err || !user) {
+         return res.status(401).json({
+            error: "User not found.",
+         });
+      }
+      req.profile = user;
+      next();
+   });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { allUsers } = require("../controller/user");
+const { allUsers, userById, getUser } = require("../controller/user");
 
 const router = express();
 
 router.get("/users", allUsers);
+router.get("/user/:userId", getUser);
+
+//any route containing :userId, our first execute userById()
+router.param("userId", userById);
 
 module.exports = router;
