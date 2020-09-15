721c80ff1fc4716710a8305edf417ffe11f4ac30
mainu007 <mainudev.co@gmail.com>
2020-09-15 17:12:53 +0600

    updated createPost with created by user

--- controller/post.js
+++ controller/post.js
@@ -1,4 +1,7 @@
 const Post = require("../models/post");
+const formidable = require("formidable");
+const fs = require("fs");
+const user = require("../models/user");
 
 exports.getPosts = (req, res) => {
    Post.find()
@@ -12,13 +15,35 @@ exports.getPosts = (req, res) => {
 };
 
 //create post
-exports.createPost = (req, res) => {
-   const post = new Post(req.body);
-   //save
-   post.save((err, post) => {
+exports.createPost = (req, res, next) => {
+   const form = new formidable.IncomingForm();
+   form.keepExtension = true;
+   form.parse(req, (err, fields, files) => {
       if (err) {
-         return res.status(400).json({ error: err });
+         return res.status(400).json({
+            error: "Image could be not uploaded.",
+         });
       }
-      res.json(post);
+      //create post
+      const post = new Post(fields);
+      //add posted by
+      post.postedBy = req.profile;
+      //file check
+      if (files.photo) {
+         post.photo.data = fs.readFileSync(files.photo.path);
+         post.photo.contentType = files.photo.type;
+      }
+      //user salt and dashed_password hide from user
+      req.profile.salt = undefined;
+      req.profile.dashed_password = undefined;
+      //save
+      post.save((err, result) => {
+         if (err) {
+            return res.status(400).json({
+               error: err,
+            });
+         }
+         res.json(result);
+      });
    });
 };
--- routes/post.js
+++ routes/post.js
@@ -2,16 +2,20 @@ const express = require("express");
 const { requireSignin } = require("../controller/auth");
 const { getPosts, createPost } = require("../controller/post");
 const { postValidator, validatorErrorHandler } = require("../validator");
+const { userById } = require("../controller/user");
 
 const router = express.Router();
 //router
 router.get("/posts", requireSignin, getPosts);
 router.post(
-   "/new/post",
+   "/post/new/:userId",
    requireSignin,
    postValidator,
-   validatorErrorHandler,
-   createPost
+   createPost,
+   validatorErrorHandler
 );
+
+//any route containing :userId, our first execute userById()
+router.param("userId", userById);
 
 module.exports = router;
