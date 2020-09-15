414cf8b2c8bbca3200cd28de1add61005315f8cd
mainu007 <mainudev.co@gmail.com>
2020-09-15 08:59:35 +0600

    get single user with require singin

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const { requireSignin } = require("../controller/auth");
 const { allUsers, userById, getUser } = require("../controller/user");
 
 const router = express();
 
 router.get("/users", allUsers);
-router.get("/user/:userId", getUser);
+router.get("/user/:userId", requireSignin, getUser);
 
 //any route containing :userId, our first execute userById()
 router.param("userId", userById);
